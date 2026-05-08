8b5c2639ca4f05c9d16e60f1726c651fa355569c
ancient03 <rizqieadriananto@gmail.com>
2026-05-08 14:40:11 -0400

    Final

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +10,13 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Analyzes the text string provided as an argument and returns
+    the emotion scores and the dominant emotion.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     
-    # Penanganan input kosong (Tugas 7 - Aktivitas 2)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     
@@ -21,6 +29,9 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the main application page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
